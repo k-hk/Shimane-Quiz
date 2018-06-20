6ae9e60304f55dcb3c1f90028e158527459e8ac4
k-hk <ho4gaki124@gmail.com>
2018-06-20 15:59:42 +0900

    back

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
+<html>
+<head>
+<title>島根良いものクイズ</title>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+<meta http-equiv="Content-Language" content="ja">
+<meta http-equiv="Content-Style-Type" content="text/css">
+<meta http-equiv="Content-Script-Type" content="text/javascript">
+<link rel="stylesheet" href="./quiz.css" type="text/css">
+</head>
+
+
 <?php
-/*
-print <<eot1
-  <html>
-  <head>
-  <title>島根良いものクイズ</title>
-  <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
-  <meta http-equiv="Content-Language" content="ja">
-  <meta http-equiv="Content-Style-Type" content="text/css">
-  <meta http-equiv="Content-Script-Type" content="text/javascript">
-  <link rel="stylesheet" href="./quiz.css" type="text/css">
-  </head>
-eot1;
-*/
 print "hello";
 ?>
+
+</html>
