ccf7b42931d56895fad2137bf3d7cd3d5c313963
k-hk <ho4gaki124@gmail.com>
2018-06-20 17:36:10 +0900

    back

--- index.php
+++ index.php
@@ -23,16 +23,14 @@
 島根良いものクイズ
 </taito>
 <pre>
-<a href="quiz.php"><br><br><br>スタート</a>
+<a href="quiz.php"><br><br>スタート</a>
 
 <?php
   $cornum=1;
-  print '<a href="coupon.php"><br>累計'.$cornum.'問正解　　　特典はここをクリック</a>';
+  print '<a href="coupon.php">累計'.$cornum.'問正解　　　特典はここをクリック</a>';
 ?>
 
-言語変更
-<a href="index.php">   日本語</a>
-<a href="index_eng.php">   English</a>
+言語変更<a href="index.php">   日本語</a><a href="index_eng.php">   English</a>
 </pre>
 </h1>
 </body>
