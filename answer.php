e797c44fd57cbc4e46aa9479b35e21e2aaf8532d
k-hk <ho4gaki124@gmail.com>
2018-06-20 20:06:45 +0900

    back

---
+++ answer.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+<title>島根良いものクイズ</title>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+<meta http-equiv="Content-Language" content="ja">
+<meta http-equiv="Content-Style-Type" content="text/css">
+<meta http-equiv="Content-Script-Type" content="text/javascript">
+<link rel="stylesheet" href="./quiz.css" type="text/css">
+<style type="text/css">
+<!--
+  a:link {color:white;}
+  a:visited{color:white;} 
+  a:hover{color:white;} 
+  a:active{color:white;}
+  a {text-decoration:none;} 
+-->
+</style>
+</head>
+<body>
+<h1>
+<?php
+  session_start();
+  if($_GET['id']==1)
+  {
+    print "<taito><font color="red">○</fonr></taito><br>";
+  }
+  else
+  {
+    print "<taito><font color="blue">×</fonr></taito><br>";
+  }
+  $a="シジミ";
+  print "A.".$_SESSION['qnum'].$a."<br>";
+  
+
+?>
+
+
+</h1>
+</body>
+</html>
