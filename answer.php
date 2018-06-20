39c7ef627127d78d8e0c6977b5beb405e8faba5c
k-hk <ho4gaki124@gmail.com>
2018-06-20 21:19:21 +0900

    back

--- answer.php
+++ answer.php
@@ -33,7 +33,7 @@
     print '<img src="./image/mark_batsu.png" alt="batu" title="batu"><br>';
   }
 
-  print '<img src="./image/shijimi.png" alt="ans" title="ans"  align="middle">';
+  print '<img src="./image/shijimi.png" alt="ans" title="ans"  align="right">';
   print "<pre>A.".$_SESSION['qnum']."　".$a."</pre><br>";
   
   
