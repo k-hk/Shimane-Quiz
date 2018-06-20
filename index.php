b01bca5194c916246e3f2225055c7f41cce5c118
k-hk <ho4gaki124@gmail.com>
2018-06-20 15:50:44 +0900

    back

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+print <<eot1
+  <html>
+  <head>
+  <title>島根良いものクイズ</title>
+  <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+  <meta http-equiv="Content-Language" content="ja">
+  <meta http-equiv="Content-Style-Type" content="text/css">
+  <meta http-equiv="Content-Script-Type" content="text/javascript">
+  <link rel="stylesheet" href="./quiz.css" type="text/css">
+  </head>
+eot1;
+
+?>
