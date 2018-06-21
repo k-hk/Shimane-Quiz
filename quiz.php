e1a75eb48ad9ff5d46f01f0add8514ee67d7b50d
unknown <ho4gaki124@gmail.com>
2018-06-21 14:28:26 +0900

    demo

--- quiz.php
+++ quiz.php
@@ -32,6 +32,7 @@
   array_push($_SESSION['endque'],$_SESSION['qn']);
   print_r($_SESSION['endque']);
   $sql = 'select question,ans1,ans2,ans3,tans from quiz where num='.$_SESSION['qn'].';';
+  print $sql;
   $_SESSION['que']=$pdo->query($sql);
 
   print $_SESSION['que']['question'];
