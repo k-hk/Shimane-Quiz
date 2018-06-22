8501b035336d432e9d900ce32953a38897549d83
unknown <ho4gaki124@gmail.com>
2018-06-22 11:29:57 +0900

    sql

--- answer.php
+++ answer.php
@@ -32,7 +32,7 @@
     print '<img src="./image/mark_batsu.png" alt="batu" title="batu"><br><br>';
   }
 
-  print '<p><img src="./image/shijimi.png" alt="ans" title="ans"></p>';
+  print '<p><img src="./image/shijimi.png" alt="ans" align="center"></p>';
   print "<pre><p>A".$_SESSION['qnum'].".　".$_SESSION['que']["ans{$_SESSION['que'][tans]}"]."</p></pre><br><br>";
   
   
