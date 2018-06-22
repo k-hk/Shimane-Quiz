2f45f5d87547d4afe8c4fd131d05f215b8e207ef
unknown <ho4gaki124@gmail.com>
2018-06-22 14:33:10 +0900

    sql

--- index_eng.php
+++ index_eng.php
@@ -52,7 +52,8 @@ Quiz<br>
 ?>
 
 
-Language selection  <a href="index.php">日本語</a>     <a href="index_eng.php">English</a>         </pre>
+
+Language selection       <a href="index.php">日本語</a>     <a href="index_eng.php">English</a>         </pre>
 </eng>
 </h1>
 </body>
