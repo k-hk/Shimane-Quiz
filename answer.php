c5de5df812454ce03542ab1c9b0a9bd8f3ce27b6
unknown <ho4gaki124@gmail.com>
2018-06-22 11:33:24 +0900

    sql

--- answer.php
+++ answer.php
@@ -32,7 +32,8 @@
     print '<img src="./image/mark_batsu.png" alt="batu" title="batu"><br><br>';
   }
 
-  print '<p><img src="./image/shijimi.png" alt="ans"><pre>A".$_SESSION['qnum'].".　".$_SESSION['que']["ans{$_SESSION['que'][tans]}"]."</p></pre><br><br>';
+  print '<p><img src="./image/shijimi.png" alt="ans">';
+  print "<pre>A".$_SESSION['qnum'].".　".$_SESSION['que']["ans{$_SESSION['que'][tans]}"]."</p></pre><br><br>";
   
   
   if($_SESSION['qnum']>=5)
