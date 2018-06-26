026829325ac520fa134eba8b658cbdf8838f4be0
k-hk <ho4gaki124@gmail.com>
2018-06-26 20:25:45 +0900

    demo

--- answer.php
+++ answer.php
@@ -32,8 +32,8 @@
     print '<img src="./image/mark_batsu.png" alt="batu" title="batu" hspace="100" align="top">';
   }
 
-  print '<img src="./image/'.$_SESSION['img'][name].'" alt="ans" height=300 width=300></ans><br><br>';
-  print '<pre><ans>A'.$_SESSION['qnum'].'.　'.$_SESSION['que']["ans{$_SESSION['que'][tans]}"].'</pre>';
+  print '<img src="./image/'.$_SESSION['img'][name].'" alt="ans" height=300 width=300><br><br>';
+  print '<pre>A'.$_SESSION['qnum'].'.　'.$_SESSION['que']["ans{$_SESSION['que'][tans]}"].'</pre>';
   
   if($_SESSION['qnum']>=5)
   {
